494a40a998c53d3c9c2f2e21f1ffcc71270dda9f
Blade21Shade <blade21shade@gmail.com>
2026-02-05 14:09:22 -0600

    Update GameEngine with click functionality and update initialization logic, also tests
    GameEngine.js
    - Initialization now just sets up variables, but doesn't set them
    - Create initializeGameState()
     - Does the actual initialization of variables and DOM
      - This was done mostly for testing purposes, but may have usefulness later
    - Create getIDNumberOfClickedCell(event)
     - Gets the ID number of the cell that the event has as a target and returns it
     - If the event target is invalid, returns -1
    - Create processIdNumberOnBoard(idNum, board)
    - Attempts to fire at board by converting idNum to a coordinate and then firing at it
    - Returns true if the shot was a hit or miss, otherwise returns false
     - Effectively, false = need to redo the click that generated the bad idNum
    - Commented out event listener
     - This lays out some future work for GameEngine, but with my plans for the program as a whole it didn't make sense to include it at the moment
    
    GameEngine.test.js
    - Sets up boards before testing, this is a near copy and paste from DOMManipulation.test.js
    - getIDNumberOfClickedCell tests
     - Sets up some dummy events for the tests to use
     - Pass tests
      - Cell number: 5
      - Cell number: 23
     - Clean-fail tests
      - These tests don't throw errors, but return the bad/fail value -1
      - Bad classList
    - processClick tests
    - All tests use a Gameboard object
     - Pass tests
      - idNumber: 5
      - idNumber: 23
     - Clean-fail tests
      - These return false
      - idNumber: -1; below 0-99 range
      - idNumber: 100; above 0-99 range
      - idNumber: 5, 5; attempt to hit already fired at spot

--- GameEngine.js
+++ GameEngine.js
@@ -1,13 +1,120 @@
 import Ship from "./Ship.js"
 import Gameboard from "./Gameboard.js"
 import Player from "./Player.js"
+import * as DOMManipulation from "./DOMManipulation.js"
 
-let gameBoardSize = 10;
-let playerTurn = 1; // Swaps between 1 and 2
+let gameBoardSize;
+let playerTurn;
 
-let player1Board = new Gameboard(gameBoardSize);
-let player2Board = new Gameboard(gameBoardSize);
+let player1Board;
+let player2Board;
 
-let player1 = new Player(player1Board);
-let player2 = new Player(player2Board);
+let player1;
+let player2;
 
+let board1Element;
+let board2Element;
+
+/**
+ * Sets up variables needed for the game to run and initial DOM setup
+ */
+function initializeGameState() {
+    gameBoardSize = 10;
+    playerTurn = 1; // Swaps between 1 and 2
+
+    player1Board = new Gameboard(gameBoardSize);
+    player2Board = new Gameboard(gameBoardSize);
+
+    player1 = new Player(player1Board);
+    player2 = new Player(player2Board);
+
+    // These do not map to player1 and player2, they change based on the current player
+    board1Element = DOMManipulation.grabBoardElement(1); // Show the active player's board with their ships and shots from the opposing player
+    board2Element = DOMManipulation.grabBoardElement(2); // Shows the opposing player's board without ships and the active player's shots
+
+    DOMManipulation.initializeBoardElements();
+}
+
+/**
+ * Attempts to get the idNumber from a cell on the "shoot at" board
+ * @param {Event} event The event who's target is the cell in the DOM
+ * @returns cell is marked as foe: The idNumber of the cell
+ * @returns cell isn't marked as foe: -1 
+ */
+function getIDNumberOfClickedCell(event) {
+    let clickedElement = event.target;
+    let idString = "";
+    let idNum = -1;
+
+    // Only evaluate cells which haven't been shot at
+    if (clickedElement.classList.contains("cell") && clickedElement.classList.contains("foe")) {
+        idString = clickedElement.id;
+        
+        // Cell id's are in the form: boardX-cellYZ ; YZ can be < 10, in which case it only has one character for the number
+        if (isNaN(idString.at(-2))) { // If the second to last character isn't a number only grab the last character
+            idNum = Number(idString.at(-1));
+        } else {
+            idNum = Number(idString.at(-2) + idString.at(-1));
+        }
+    }
+
+    return idNum;
+}
+
+/**
+ * Attempts to process the given cell idNumber onto the given board
+ * @param {number} idNum The idNumber of a cell
+ * @param {Gameboard} board The board to process the idNumber against
+ * @returns True if the click was valid (hit or miss), or False if it was invalid
+ */
+function processIDNumberOnBoard(idNum, board) {
+    let clickCouldBeProcessed = false;
+
+    // If idNum isn't in the valid range, don't do anything
+    if (idNum < 0 || idNum > 99) {
+        return clickCouldBeProcessed;
+    }
+
+    // Find the coordinate of this id
+    // See note in DOMManipulation.js's fillBoardElementShips(...) for the logic of this math; this math is different than that math
+    // The math here constructs a position from a cell number, the math in fillBoardElementShips() constructs a cell number from a position
+    let x = idNum % 10;
+    let y = Math.floor(idNum/10);
+    let coord = [x,y];
+
+    // Actual processing
+    let hitVal = -10;
+    try {
+        hitVal = board.fireAtBoard(coord);
+        if (hitVal === -1 || hitVal === 1) { // Hit or miss
+            clickCouldBeProcessed = true;
+        }
+    } catch (error) {
+        return clickCouldBeProcessed; // If fireAtBoard() receives a coordinate that has already been shot at it throws an error, in which case do nothing
+    }
+
+    return clickCouldBeProcessed;
+}
+
+/*
+board2Element.addEventListener("click", (e) => {
+        // Process what the click did to the game
+        let board = playerTurn === 1 ? player2Board : player1Board;
+
+        let cellNumberClicked = getIDNumberOfClickedCell(e);
+        let couldBeProcessed = processIDNumberOnBoard(cellNumberClicked, board);
+        if (couldBeProcessed) {
+            let allShipsSunk = board.getAllShipsSunk();
+        
+            // See if the game should end
+            if (allShipsSunk) {
+                // End of game logic
+                return;
+            }
+
+            // The game didn't end, prepare for player swap
+        }
+    });
+*/
+
+export {getIDNumberOfClickedCell, processIDNumberOnBoard, initializeGameState}
---
+++ GameEngine.test.js
@@ -0,0 +1,154 @@
+/**
+ * @jest-environment jsdom
+ */
+
+jest.mock("./DOMManipulation", () => ({
+    grabBoardElement: jest.fn(()=>"testVal")
+}));
+
+import { getIDNumberOfClickedCell, processIDNumberOnBoard} from "./GameEngine.js";
+
+import { grabBoardElement } from "./DOMManipulation.js";
+
+import Gameboard from "./Gameboard.js";
+
+// Boards for the tests to use
+const board1 = document.createElement("div");
+board1.id = "board1";
+document.body.appendChild(board1);
+
+const board2 = document.createElement("div");
+board2.id = "board2";
+document.body.appendChild(board2);
+
+const cell = document.createElement("div");
+cell.classList.add("cell");
+
+// Setup the boards for use
+for (let i = 0; i < 100; i++) {
+    let clone = cell.cloneNode();
+        clone.classList.toggle("friend");
+        clone.id = `board1-cell${i}`;
+        board1.appendChild(clone);
+
+        clone = cell.cloneNode();
+        clone.classList.toggle("foe");
+        clone.id = `board2-cell${i}`;
+        board2.appendChild(clone);
+}
+
+// Variables the tests can use 
+const board1Array = [
+    [1,0,0,0,0,0,0,1,0,-1],
+    [0,1,0,0,0,0,0,1,0,-1],
+    [0,0,0,0,0,0,0,0,0,0], // This row and below are unused, they are present so fillBoardElement() still works
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
+];
+
+const board1Gameboard = new Gameboard(10);
+
+const shipCoordinates1 = [
+    [[1,0], [2,0]],
+    [[7,0], [7,1]],
+    [[8,0], [8,1]]
+];
+
+// Reset the board cell's to a clean classList for the next test
+afterEach(() => {
+    for (let i = 0; i < 100; i++) {
+        board1.children[i].classList.remove("miss", "hit", "ship");
+        board2.children[i].classList.remove("miss", "hit");
+
+        board1.children[i].classList.add("friend");
+        board2.children[i].classList.add("foe");
+    }
+});
+
+beforeEach(() => {
+    let brd1 = document.querySelector("#board1");
+    let brd2 = document.querySelector("#board2");
+
+    grabBoardElement
+    .mockImplementationOnce(()=>brd1)
+    .mockImplementationOnce(()=>brd2);
+});
+
+describe("GameEngine tests", () => {
+    describe("getIDNumberOfClickedCell tests", () => {
+        // Elements that are evaluated
+        let targetEle2Numbers = document.createElement("div");
+        targetEle2Numbers.classList.add("cell", "foe");
+        targetEle2Numbers.id = "board1-cell23";
+        
+        let targetEle1Number = document.createElement("div");
+        targetEle1Number.classList.add("cell", "foe");
+        targetEle1Number.id = "board1-cell5";
+
+        let targetEleBadClassList = document.createElement("div");
+        targetEleBadClassList.classList.add("cell", "friend");
+        // No ID as tests should fail at the classList
+
+        // Elements to represent the clicked element in the DOM
+        let clickedEle2Numbers = {
+            target: targetEle2Numbers
+        }
+
+        let clickedEle1Number = {
+            target: targetEle1Number
+        }
+
+        let clickedEleBadClassList = {
+            target: targetEleBadClassList
+        }
+
+        describe("Pass tests", () => {
+            test("Cell number: 5", () => {
+                expect(getIDNumberOfClickedCell(clickedEle1Number)).toBe(5);
+            });
+            
+            test("Cell number: 23", () => {
+                expect(getIDNumberOfClickedCell(clickedEle2Numbers)).toBe(23);
+            });
+        }); 
+
+        describe("Clean-fail tests\nThese tests return a value, but that value represents a fail", ()=> {
+            test("Bad classList - bad classes still return a value: -1", () => {
+                expect(getIDNumberOfClickedCell(clickedEleBadClassList)).toBe(-1);
+            });
+        });
+
+    });
+
+    describe("processClick tests", () => {
+        describe("Pass tests", () => {
+            test("idNumber: 5", () => {
+                expect(processIDNumberOnBoard(5, board1Gameboard)).toBe(true); // Click is processed
+            });
+            
+            test("idNumber: 23", () => {
+                expect(processIDNumberOnBoard(23, board1Gameboard)).toBe(true); // Click is processed
+            });
+        });
+
+        describe("Clean-fail tests - return false", () => {
+            test("idNumber: -1; below 0-99 range", () => {
+                expect(processIDNumberOnBoard(-1, board1Gameboard)).toBe(false);
+            });
+            
+            test("idNumber: 100; above 0-99 range", () => {
+                expect(processIDNumberOnBoard(23, board1Gameboard)).toBe(false);
+            });
+
+            test("idNumber: 5, 5; attempt to hit already fired at spot", () => {
+                processIDNumberOnBoard(5, board1Gameboard);
+                expect(processIDNumberOnBoard(5, board1Gameboard)).toBe(false);
+            });
+        }); 
+    });
+});
