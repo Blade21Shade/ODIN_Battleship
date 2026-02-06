a1c60ea61e49a9378bc77124190945e967ddfe92
Blade21Shade <blade21shade@gmail.com>
2026-02-06 14:57:36 -0600

    Split commented out event listener into logical pieces
    This wasn't aimed to create/change logic  inside the comment much, instead this was meant to break the pieces of the logic up into more reasonable parts.
    
    There are no tests for these changes because none of them currently do anything to state nor return anything.
    
    GameEngine.js
    - Created preventProcessing
     - This variable is toggled true and false as users make clicks so they can't make many clicks in sequence which would cause logical issues
    - Created enableGameplayEventListener()
     - This function enables the event listener to board2 which processes user clicks
      - This just uses addEventListener, but enable was a cleaner name in case I want to disable it in the future
     - It uses an actual callback function instead of an anonymous function for future proofing
    - Created gameplayEventListenerCallback(event)
     - The callback for the event listener mentioned above
     - Processes a click if more processing is allowed to occur
      - If processing does occur, prevents more processing until all processing started from this function finishes
     - If the event could be processed, proceeds to the next game step
     - If the event couldn't be processed, alerts the user and allows more processing to occur
    - Create triggerNextGameStep(board)
     - Determines if the game should end from the state of the passed in board
     - If so, calls function to end the game
     - If not, does logic needed for next player to play their turn
      - Not implemented yet, but general process is commented
    - Create endGame()
     - Function which will do end of game things
     - Not implemented yet

--- GameEngine.js
+++ GameEngine.js
@@ -15,6 +15,8 @@ let player2;
 let board1Element;
 let board2Element;
 
+let preventProcessing = false;
+
 /**
  * Sets up variables needed for the game to run and initial DOM setup
  */
@@ -94,25 +96,72 @@ function processIDNumberOnBoard(idNum, board) {
     return clickCouldBeProcessed;
 }
 
-/*
-board2Element.addEventListener("click", (e) => {
-        // Process what the click did to the game
-        let board = playerTurn === 1 ? player2Board : player1Board;
+/**
+ * Adds the gameplay event listener to board2element
+ */
+function enableGameplayEventListener() {
+    board2Element.addEventListener("click", gameplayEventListenerCallback);
+}
 
-        let cellNumberClicked = getIDNumberOfClickedCell(e);
-        let couldBeProcessed = processIDNumberOnBoard(cellNumberClicked, board);
-        if (couldBeProcessed) {
-            let allShipsSunk = board.getAllShipsSunk();
-        
-            // See if the game should end
-            if (allShipsSunk) {
-                // End of game logic
-                return;
-            }
+/**
+ * This function handles what happens when the game starts and players start clicking the "shoot at" board to fire at their opponent
+ * @param {Event} event The event coming from the event listener this callback is attached to
+ */
+function gameplayEventListenerCallback(event) {
+    // If the previous action is still processing prevent more actions from processing
+    if (preventProcessing) {
+        return;
+    } else {
+        preventProcessing = true;
+    }
 
-            // The game didn't end, prepare for player swap
-        }
-    });
-*/
+    // Process what the click did to the game
+    let board = playerTurn === 1 ? player2Board : player1Board;
 
-export {getIDNumberOfClickedCell, processIDNumberOnBoard, initializeGameState}
+    let cellNumberClicked = getIDNumberOfClickedCell(event.target);
+    let couldBeProcessed = processIDNumberOnBoard(cellNumberClicked, board);
+    if (couldBeProcessed) {
+        triggerNextGameStep(board);
+    } else {
+        alert("Invalid click, try another cell!");
+        preventProcessing = false;
+    }
+}
+
+/**
+ * Causes the next game step to occur: either end the game, or swap players and continue
+ * @param {Gameboard} board
+ */
+function triggerNextGameStep(board) {
+    let allShipsSunk = board.getAllShipsSunk();
+    
+    // See if the game should end
+    if (allShipsSunk) {
+        endGame();
+        return;
+    }
+
+    // The game didn't end, setup for player swap
+
+    /**
+     * ? Obfuscate screen so players don't accidentally see each other's boards
+     * Reset each DOM board
+     * Fill DOM boards with information for next player
+     * Swap players
+     */
+
+    // Swap players
+    // playerTurn = playerTurn === 1 ? 2 : 1; // Swap which player is active
+    // preventProcessing = false;
+}
+
+/**
+ * Function that will handle end of game logic
+ */
+function endGame() {
+
+}
+
+
+
+export {getIDNumberOfClickedCell, processIDNumberOnBoard, initializeGameState, enableGameplayEventListener}
